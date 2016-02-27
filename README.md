260eb9938bf1b6f66216e5e2bcfe6ecf44777ada
Christopher Su <christophersu9@gmail.com>
2016-02-26 18:51:02 -0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# scrape
+Quickly scrape web pages from the command line. Outputs results to stdout.
+
+## Example
+```bash
+$ scrape http://christopher.su ".home-nav span"
+resume
+linkedin
+v1
+latest
+archive
+email
+twitter
+pgp
+github
+list
+grid
+```
---
+++ scrape.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "os"
+
+  "github.com/codegangsta/cli"
+  "github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "scrape"
+  app.Usage = "scrapes a web page for a given selector"
+  app.Action = func(c *cli.Context) {
+    if len(c.Args()) != 2 {
+      log.Fatal("Need 2 arguments.")
+    }
+    doc, err := goquery.NewDocument(c.Args()[0]) 
+    if err != nil {
+      log.Fatal(err)
+    }
+    doc.Find(c.Args()[1]).Each(func(i int, s *goquery.Selection) {
+      fmt.Println(s.Text())
+    })
+  }
+  app.Run(os.Args)
+}
+
